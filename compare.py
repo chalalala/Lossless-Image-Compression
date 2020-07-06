929010275a3cd97727544a3c22571b1395e950aa
chalalala <neko_hime1212@yahoo.com>
2020-07-06 21:33:00 +0700

    Upload code to compare images

---
+++ compare.py
@@ -0,0 +1,17 @@
+import cv2
+import matplotlib.pyplot as plt
+
+def compare(paths,titles):
+    plt.figure('Comparison')
+    
+    for i in range(2):
+        img = cv2.imread(paths[i])
+        plt.subplot(1,2,i+1)
+        plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+        #plt.xticks([]),plt.yticks([])
+        plt.axis("off")
+        plt.title(titles[i])
+    
+    plt.tight_layout(pad=.5)
+    plt.show()
+
