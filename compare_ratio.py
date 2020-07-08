4fb09e208e117ef9493007f1e9bc148158ba9b2b
chalalala <neko_hime1212@yahoo.com>
2020-07-09 00:02:40 +0700

    Edit command to display file names

--- compare_ratio.py
+++ compare_ratio.py
@@ -6,6 +6,5 @@ code_name = sys.argv[1]
 
 for file_name in os.listdir('images'):
     path = os.path.join('images',file_name)
-    print(file_name)
+    os.system('echo ' + file_name)
     os.system(' '.join(['python',code_name + '.py',path]))
-    print()
