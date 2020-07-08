f3f530842f43811e7b6245eb6bb5219f5c608fc8
chalalala <neko_hime1212@yahoo.com>
2020-07-08 23:39:54 +0700

    Upload code to run on all files in the images folder

---
+++ compare_ratio.py
@@ -0,0 +1,11 @@
+import sys,os
+
+# To run: python compare_ratio.py Huffman
+
+code_name = sys.argv[1]
+
+for file_name in os.listdir('images'):
+    path = os.path.join('images',file_name)
+    print(file_name)
+    os.system(' '.join(['python',code_name + '.py',path]))
+    print()
